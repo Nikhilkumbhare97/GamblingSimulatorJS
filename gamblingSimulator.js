53910957a12c5025e07697e1d182136c236eb647
nikhilkumbhare <nikhilk190697@gmail.com>
2021-05-12 17:28:51 +0530

    Added: Bet For Win or Lose

--- gamblingSimulator.js
+++ gamblingSimulator.js
@@ -2,6 +2,22 @@ console.log("Welcome To Gambling Simulation Problem");
 
 const STAKE = 100;
 const BET = 1;
+const WON = 1;
 
 console.log("Stake : " + STAKE);
 console.log("Bet : " + BET);
+
+let total;
+
+game = () => {
+    total = STAKE;
+    let gameCheck = Math.floor(Math.random() * 10 % 2);
+    if(gameCheck == WON){
+        total += BET;
+        console.log("Game Won")
+    }else{
+        total -= BET;
+        console.log("Game Lose")
+    }
+    console.log("Total : " + total);}
+game();

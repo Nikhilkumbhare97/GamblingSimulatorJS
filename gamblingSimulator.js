caa33c2eddfbb644c78ddefd1bc810f8b10389ca
nikhilkumbhare <nikhilk190697@gmail.com>
2021-05-12 18:13:57 +0530

    Added: After 20 Days Playing get Total Amount Won or Lose

--- gamblingSimulator.js
+++ gamblingSimulator.js
@@ -5,6 +5,7 @@ const BET = 1;
 const WON = 1;
 const LOSE_CONDITION = STAKE / 2;
 const WIN_CONDITION = STAKE + STAKE / 2;
+const TOTAL_DAYS = 20;
 
 console.log("Stake : " + STAKE);
 console.log("Bet : " + BET);
@@ -12,6 +13,8 @@ console.log("Bet : " + BET);
 let total;
 var winTime = 0;
 var loseTime = 0;
+var totalAmountWin = 0;
+var totalAmountLose = 0;
 
 game = () => {
     total = STAKE;
@@ -25,8 +28,23 @@ game = () => {
             loseTime++;
         }
     }
-    console.log("Total : " + total);
+    return total;
+}
+
+daysPlaying = () => {
+    for (let day = 1; day <= TOTAL_DAYS; day++) {
+        let Amount = 0;
+        Amount = game();
+        if (Amount > STAKE) {
+            totalAmountWin += (Amount - STAKE);
+        } else {
+            totalAmountLose += (STAKE - Amount);
+        }
+        console.log("Total for Day " + day + " is : " + Amount);
+    }
+    console.log("Total Amount Won : " + totalAmountWin);
+    console.log("Total Amount Lose : " + totalAmountLose);
     console.log("Total Win Time : " + winTime);
     console.log("Total Lose Time : " + loseTime);
 }
-game();
+daysPlaying();
